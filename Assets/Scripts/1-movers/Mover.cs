1519ad3f7bfc9c0a0b69d691b397b78460eb028b
DorBaram <73188591+DorBaram@users.noreply.github.com>
2023-04-18 21:14:01 +0300

    First Commit

---
+++ Assets/Scripts/1-movers/Mover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/**
+ * This component moves its object in a fixed velocity.
+ * NOTE: velocity is defined as speed+direction.
+ *       speed is a number; velocity is a vector.
+ */
+public class Mover: MonoBehaviour {
+    [Tooltip("Movement vector in meters per second")]
+    [SerializeField] Vector3 velocity;
+
+    int downBorder = -7;
+    int upBorder = 7;
+
+    void Update() {
+        transform.position += velocity * Time.deltaTime;
+        if(transform.position.y < downBorder || transform.position.y > upBorder)
+        {
+            Destroy(gameObject);
+            //Score.AddScore(Score.pointsForCar);
+        }
+    }
+
+    public void SetVelocity(Vector3 newVelocity) {
+        this.velocity = newVelocity;
+    }
+}
